5677a994a21ef2b1b38c6e0c987688f78171b980
agroenleer <alan.groenleer@gmail.com>
2025-10-17 12:22:52 -0400

    Add files via upload

---
+++ # Book page 34-5.py
@@ -0,0 +1,19 @@
+# Book page 34-5
+
+print ("ClydeBank Coffee Shop, version 1.00")
+print ("Copyright (C) 2025")
+print ("Collect some information")
+#Get Name and Shop Name")
+name = input("Enter your name: ")
+shop_name = input("What do you want to call your shop? : ")
+print ("Hello\nThank You", name, "Let's get some initial pricing.\n")
+
+#Get initial price of a cup of coffee
+cup_price =  input("Enter the initial price of a cup of coffee: ")
+
+#Display what we have so far
+
+print ("\nGreat!. Here is what we have so far.\n")
+print ("Your name is " + name + " and you're opening " + shop_name + "!")
+print ("Your first cup of coffee will cost $" + cup_price + "\n")
+
